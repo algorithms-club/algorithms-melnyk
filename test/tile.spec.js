939dce7175f8b4ead4244bc1f819bcf170d6047a
yuchiko <iyuchiko@gmail.com>
2017-03-30 00:14:07 +0300

    fix tasks description

--- test/tile.spec.js
+++ test/tile.spec.js
@@ -4,13 +4,13 @@ const algolib = require('../index.js');
 
 describe('#coverByTile', function() {
 
-  it('should count amunt of tiles to cower area (integer)', function() {
+  it('should count amount of tiles to cover area (integer)', function() {
     let tilesAmount = algolib.coverByTile(5, 3, 2);
 
     tilesAmount.should.be.eql(6);
   });
 
-  it('should count amunt of tiles to cower area (float)', function() {
+  it('should count amount of tiles to cover area (float)', function() {
     let tilesAmount = algolib.coverByTile(0.5, 1.3, 0.2);
 
     tilesAmount.should.be.eql(21);
